fe7cdcccedcc66b7fc053669b77e626da977ad34
Mirzet Zukic <zukicmirzet94@gmail.com>
2023-03-02 22:12:08 +0100

    Add engName property to models

--- source/model/invokerUi.ts
+++ source/model/invokerUi.ts
@@ -3,6 +3,7 @@ import { Baseclass } from "@flexicore/flexicore-client";
 
 export interface InvokerUI extends Baseclass {
     hebName?: string;
+    engName?: string;
     tooltip?: string;
     tooltipHeb?: string;
     canonicalName?: string;
--- source/model/invokerUiCreate.ts
+++ source/model/invokerUiCreate.ts
@@ -5,6 +5,7 @@ export interface InvokerUICreate extends BaseclassCreate {
     unsetProperties?: string[];
     canonicalName?: string;
     hebName?: string;
+    engName?: string;
     tooltip?: string;
     tooltipHeb?: string
 }
